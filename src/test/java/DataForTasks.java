49fa595b8047bf820f53d010bbfc0dc6c1f3e5a7
DmitryLapitsky <56531706+DmitryLapitsky@users.noreply.github.com>
2019-12-27 01:49:37 +0300

    comments
    comments

--- src/test/java/DataForTasks.java
+++ src/test/java/DataForTasks.java
@@ -4,32 +4,32 @@ import org.testng.annotations.DataProvider;
 public class DataForTasks {
 
     public static final String URL_GOOGLE = "https://www.google.com/";
-    public final int pages = 5;
-    public final int ms = 10;
+    public final int pages = 5; //количество страниц, которое надо обойти
+    public final int ms = 10; //максимальное время, когда браузер ожидает появления элемента
 
 
     public static final String PLAY_GOOGLE = "play.google.com";
 
-    public static class GooglePage{
-        public static By input = new By.ByCssSelector("input.gLFyf");
-        public static By searchButton = new By.ByXPath(".//*[@class='FPdoLc tfB0Bf']/*/input[@aria-label='Поиск в Google']");
-        public static By getElement(String s){
+    public static class GooglePage{//класс страница гугла, где ниже элементы для поиска информации, кнопок и полей ввода
+        public static By input = new By.ByCssSelector("input.gLFyf");//строка поиска
+        public static By searchButton = new By.ByXPath(".//*[@class='FPdoLc tfB0Bf']/*/input[@aria-label='Поиск в Google']");//кнопка найти в гугл
+        public static By getElement(String s){//метод возрата значения By по внутреннему названию элемента
             return new By.ByXPath("//*[contains(text(),'"+ s +"')]");
         }
-        public static By googleLogo = new By.ByXPath("//img[@alt='Google']");
-        public static By iviLinks = new By.ByXPath("//img/../..//*[text()='ivi.ru']|//*[text()='ivi.ru']");
+        public static By googleLogo = new By.ByXPath("//img[@alt='Google']");//лого гугла
+        public static By iviLinks = new By.ByXPath("//img/../..//*[text()='ivi.ru']|//*[text()='ivi.ru']");//ссылки на ivi в картинках
 
-        public static By ratingText = new By.ByXPath("//*[contains(text(),'play.google')]/../../../..//*[contains(text(),'Рейтинг')]");
-        public static By wikiIvi = new By.ByXPath("//*[contains(text(),'wikipedia.org')][contains(text(),'Ivi')]|//*[contains(text(),'wikipedia.org')][contains(text(),'ivi')]");
-        public static By currentPageNumber = new By.ByXPath("//*[@class='cur']");
+        public static By ratingText = new By.ByXPath("//*[contains(text(),'play.google')]/../../../..//*[contains(text(),'Рейтинг')]");//рейтинг приложения, потом парсится
+        public static By wikiIvi = new By.ByXPath("//*[contains(text(),'wikipedia.org')][contains(text(),'Ivi')]|//*[contains(text(),'wikipedia.org')][contains(text(),'ivi')]");// ссылка на wiki+ivi
+        public static By currentPageNumber = new By.ByXPath("//*[@class='cur']");//показывает текущую страницу поиска, для красоты
     }
 
-    public static class WikiPage{
-        public static By iviLink = new By.ByXPath("//*[@href='https://www.ivi.ru']");
+    public static class WikiPage{//класс страница википедии
+        public static By iviLink = new By.ByXPath("//*[@href='https://www.ivi.ru']");//ссылка на ivi
     }
 
-    public static class PlayMarket{
-        public static By playRating =  new By.ByXPath("//div[@class][contains(@aria-label,'Средняя оценка: ')]");
+    public static class PlayMarket{//класс страница гугл-плея
+        public static By playRating =  new By.ByXPath("//div[@class][contains(@aria-label,'Средняя оценка: ')]");//рейтинг
     }
 }
 /*
--- src/test/java/Ivi.java
+++ src/test/java/Ivi.java
@@ -14,110 +14,113 @@ import java.util.concurrent.TimeUnit;
 
 public class Ivi extends DataForTasks{
 
-    WebDriver driver;
+    WebDriver driver;//завожу переменную типа вебдрайвер
 
-    @BeforeSuite
+    @BeforeSuite// аннотация testng для выполнения следующего метода перед началом тестирования (иерархия соответствует файлу local.xml)
     public void getDriver(){
-        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(ms, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(ms, TimeUnit.SECONDS);
-        driver.manage().timeouts().setScriptTimeout(ms, TimeUnit.SECONDS);
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");//указываю, где искать браузер
+        driver = new ChromeDriver();//инициализация браузера
+        driver.manage().timeouts().implicitlyWait(ms, TimeUnit.SECONDS);//автопопытки найти элементы на странице
+        driver.manage().timeouts().pageLoadTimeout(ms, TimeUnit.SECONDS);//ожидание загрузки страницы
+        driver.manage().timeouts().setScriptTimeout(ms, TimeUnit.SECONDS);//ожидание отработки скриптов
     }
 
-    @BeforeMethod
-    public void getSeach(){
-        driver.get(URL_GOOGLE);
-        driver.findElement(GooglePage.input).sendKeys("ivi");
-        click(GooglePage.input);
-        click(GooglePage.googleLogo);
-        click(GooglePage.searchButton);
+    @BeforeMethod// аннотация testng для выполнения следующего метода перед каждым тестом
+    public void getSeach(){//первые шаги тестов одинаковы, поэтому в отдельный метод
+        driver.get(URL_GOOGLE);//открытие сайта google
+        driver.findElement(GooglePage.input).sendKeys("ivi");//ввод в строку поика ivi
+        click(GooglePage.googleLogo);//так как выпадающий список скрывает кнопку поиска, то жму по логотипу гугла, чтобы список пропал
+        click(GooglePage.searchButton);//нажимаю кнопку поиска
     }
 
-    @BeforeMethod
+    @BeforeMethod//после каждого теста возвращаюсь к исходной странице гугла
     public void getBack(){
         driver.get(URL_GOOGLE);
     }
 
-    @AfterSuite
+    @AfterSuite//после тестов закрываю хром
     public void closeDriver(){
         if(driver!=null)
             driver.close();
     }
 
-    @Test(groups = "homeWork",description = "проверка русской локали браузера")
+    @Test(groups = "homeWork",description = "проверка русской локали браузера")//первый тест на проверку локали браузера, если она английская, то тесты дальше не идут (зависимость по groups = "homeWork")
     public void browserLocale() {
-        driver.findElement(GooglePage.getElement("Картинки"));
+        driver.findElement(GooglePage.getElement("Картинки"));//если находится элемент с текстом картинки, то все хорошо
     }
 
-    @Test(dependsOnGroups = "homeWork.*", description = "проверка, что не менее 3 больших картинок в выдаче google ведут на сайт ivi.ru")
+    @Test(dependsOnGroups = "homeWork.*", description = "проверка, что не менее 3 больших картинок в выдаче google ведут на сайт ivi.ru")//зависит от выполнения теста browserLocale, если он провалится, то этот не запустится
     public void imagesOfIviInGooglePictures(){
-        click(GooglePage.getElement("Картинки"));
-        click(GooglePage.getElement("Инструменты"));
-        wait(GooglePage.getElement("Размер"));
-        click(GooglePage.getElement("Размер"));
-        click(GooglePage.getElement("Больш"));
-        Assert.assertTrue(driver.findElements(GooglePage.iviLinks).size()>2, "всего " + driver.findElements(GooglePage.iviLinks).size() + " картинок ведут на ivi.ru");
+        click(GooglePage.getElement("Картинки"));//перехожу в раздел картинок
+        click(GooglePage.getElement("Инструменты"));//открываю инструменты
+        wait(GooglePage.getElement("Размер"));//ожидаю появления по раскрывающегося списка "размер" - появляется не всегда, поэтому на всякий
+        click(GooglePage.getElement("Размер"));//щелкаю по нему
+        click(GooglePage.getElement("Больш"));//нажимаю по большим картинкам. Вставил без окончания, т.к. иногда гугл склоняет это слово:(
+        Assert.assertTrue(driver.findElements(GooglePage.iviLinks).size()>2, "всего " + driver.findElements(GooglePage.iviLinks).size() + " картинок ведут на ivi.ru");//ассерт не выполнении условия и при ошибке пишет, что не так
     }
 
     @Test(dependsOnGroups = "homeWork.*", description = "проверка совпадения рейтинга приложения в результатах поиска с рейтингом в play-маркете")
     public void playIvi(){
-        SoftAssert asert = new SoftAssert();
-        for(int i = 0; i < pages; i++) {
+        SoftAssert asert = new SoftAssert();//так как google.play-ев может найтись много и где-то будут ошибки, ввожу софт-ассерт, чтобы тест нашел все ошибки и выдал их скопом, а не прекратил выполнение на первой ошибке
+        for(int i = 0; i < pages; i++) {//цикл по количеству страниц
             try {
-                int plays = finds(GooglePage.getElement(PLAY_GOOGLE)).size();
-                if(plays>0) {
-                    for (int j = 0; j < plays; j++) {
-                        String googleRating = finds(GooglePage.ratingText).get(j).getText().split(" ")[1];
-                        String url = driver.getCurrentUrl();
-                        finds(GooglePage.getElement(PLAY_GOOGLE)).get(j).click();
-                        String playRating = driver.findElement(PlayMarket.playRating).getText();
-                        asert.assertEquals(googleRating.equals(playRating),true,"рейтинг play в поиске " + googleRating + " не совпадает c рейтингом в магазине play " + playRating);
-                        driver.get(url);
+                int plays = finds(GooglePage.getElement(PLAY_GOOGLE)).size();//находит массив элементов с гугл-плеем (может быть нулевым-ничего не найдено)
+                if(plays>0) {//если что-то найдено
+                    for (int j = 0; j < plays; j++) {//в цикле по найденному
+                        String googleRating = finds(GooglePage.ratingText).get(j).getText().split(" ")[1];//сохраняю рейтинг на странице поиска на соответствующей позиции, где был найден гугл-плей
+                        String url = driver.getCurrentUrl();//сохраняю url
+                        finds(GooglePage.getElement(PLAY_GOOGLE)).get(j).click();//перехожу на страницу гугл-плея
+                        String playRating = driver.findElement(PlayMarket.playRating).getText();//сохраняю рейтинг гугл-плея
+                        asert.assertEquals(googleRating.equals(playRating),true,"рейтинг play в поиске " + googleRating + " не совпадает c рейтингом в магазине play " + playRating);//сравниваю оценки, ошибка, если не одинаковы
+                        driver.get(url);// возвращаюсь туда, откуда пришел
                     }
                 }
-            } catch (Exception e) {
-                System.out.println("на странице " + driver.findElement(GooglePage.currentPageNumber).getText() + " не найден элемент play.google.com");
-            } finally {
-                click(GooglePage.getElement("Следующая"));
+                else{
+                    System.out.println("на странице " + driver.findElement(GooglePage.currentPageNumber).getText() + " не найден элемент play.google.com");//если элементов гугл-плея нет, так и пишу
+                }
+            } catch (Exception e) { System.out.println(e);}//тут можно что-нибудь полезное сделать, но я не придумал
+            finally {
+                click(GooglePage.getElement("Следующая"));//в любом случае перехожу на следующую страницу
             }
         }
-        asert.assertAll();
+        asert.assertAll();//вывод накопившихся ошибок
     }
 
     @Test(dependsOnGroups = "homeWork.*", description = "проверка, что в статье ivi на wiki есть ссылка на официальный сайт ivi.ru")
     public void wikiIvi() {
-        SoftAssert asert = new SoftAssert();
-        for (int i = 0; i < pages; i++) {
-            int wikis = finds(GooglePage.wikiIvi).size();
+        SoftAssert asert = new SoftAssert();//так как wiki-ссылок может найтись много и где-то будут ошибки, ввожу софт-ассерт, чтобы тест нашел все ошибки и выдал их скопом, а не прекратил выполнение на первой ошибке
+        for (int i = 0; i < pages; i++) {//по заданному количеству страниц прохожу
+            int wikis = finds(GooglePage.wikiIvi).size();//далее аналогично гугл-плею, только без try/catch
             if (wikis > 0) {
                 for (int j = 0; j < wikis; j++) {
                     String url = driver.getCurrentUrl();
                     finds(GooglePage.wikiIvi).get(j).click();
-                    asert.assertEquals(finds(WikiPage.iviLink).size()>0, true,"на странице ivi в wiki нет ссылки на ivi");//.assertTrue(finds(GooglePage.getElement(URL_IVI)).size()>0, "на странице ivi в wiki нет ссылки на ivi");
+                    asert.assertEquals(finds(WikiPage.iviLink).size()>0, true,"на странице ivi в wiki нет ссылки на ivi");//количество ссылок на ivi должно быть больше 0, ошибка, если 0
                     driver.get(url);
                 }
                 click(GooglePage.getElement("Следующая"));
             } else {
                 System.out.println("на странице " + driver.findElement(GooglePage.currentPageNumber).getText() + " не найдена ссылка на wikipedia");
-                click(GooglePage.getElement("Следующая"));
+                click(GooglePage.getElement("Следующая"));//если не найдено wiki элементов, то перехожу на следующую страницу
             }
         }
-        asert.assertAll();
+        asert.assertAll();//вывод накопившихся ошибок
     }
 
-    public void wait(By by){
+    //небольшая оболочка над selenium-ом
+
+    public void wait(By by){//метод явного ожидания элемента, на всякий
         try{
-            (new WebDriverWait(driver, ms)).until(ExpectedConditions.presenceOfElementLocated(by));
-        }catch(Exception e){System.out.println("элемент " + by + " не найден");}
+            (new WebDriverWait(driver, ms)).until(ExpectedConditions.presenceOfElementLocated(by));//ожидание появления элемента
+        }catch(Exception e){System.out.println("элемент " + by + " не найден");}//если не найден, так и пишем
     }
 
-    public List<WebElement> finds(By by){
+    public List<WebElement> finds(By by){//метод поиска множества элементов, реализовывает вначале ожидание появления какого-либо первого элемента, а потом и выдачу всего массива элементов
         wait(by);
         return driver.findElements(by);
     }
 
-    public void click(By by){
+    public void click(By by){//метод нажатия на элемент с предварительным ожиданием его появления
         try {
             WebElement el = (new WebDriverWait(driver, ms)).until(ExpectedConditions.presenceOfElementLocated(by));
             el.click();
